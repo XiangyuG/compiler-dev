da5448cc623d6b0e6e70ba6e57df32180d7d7127
Xiangyu Gao <xg673@nyu.edu>
2023-05-18 21:07:31 -0700

    L12 from https://www.bilibili.com/video/BV1Jy4y1d7nq?p=13&spm_id_from=pageDriver

--- compiler.h
+++ compiler.h
@@ -2,6 +2,121 @@
 #define PEACHCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define S_EQ(str, str2) \
+        (str && str2 && (strcmp(str, str2) == 0))
+
+struct pos {
+    int line;
+    int col;
+    const char* filename;
+};
+
+#define NUMERIC_CASE\
+    case '0': \
+    case '1': \
+    case '2': \
+    case '3': \
+    case '4': \
+    case '5': \
+    case '6': \
+    case '7': \
+    case '8': \
+    case '9'
+
+#define OPERATOR_CASE_EXCLUSING_DIVISION \
+    case '+':                            \
+    case '-':                            \
+    case '*':                            \
+    case '>':                            \
+    case '<':                            \
+    case '^':                            \
+    case '%':                            \
+    case '!':                            \
+    case '=':                            \
+    case '~':                            \
+    case '|':                            \
+    case '&':                            \
+    case '(':                            \
+    case '[':                            \
+    case ',':                            \
+    case '.':                            \
+    case '?'
+
+#define SYMBOL_CASE \
+    case '{':       \
+    case '}':       \
+    case ':':       \
+    case ';':       \
+    case '#':       \
+    case '\\':       \
+    case ')':       \
+    case ']'       
+
+enum {
+    LEXICAL_ANALYSIS_ALL_OK,
+    LEXICAL_ANALYSIS_INPUT_ERROR
+};
+
+enum {
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_CONTENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token {
+    int type;
+    int flags;
+    struct pos pos;
+    union {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+
+    // True if there is whitespace between the token and next token
+    // i.e 
+    bool whitespace;
+    const char* between_brackets;
+};
+
+struct lex_process;
+
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process* process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process* process);
+typedef char (*LEX_PROCESS_PUSH_CHAR)(struct lex_process* process, char c);
+
+struct lex_process_functions {
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEEK_CHAR peek_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+};
+
+struct lex_process {
+    struct pos pos;
+    struct vector* token_vec;
+    struct compile_process* compiler;
+
+    /*
+    ((60))
+    */
+    int current_expression_count;
+    struct buffer* parentheses_buffer; 
+    struct lex_process_functions* function;
+
+    void* private;
+};
+
 enum {
     COMPILER_FILE_COMPILED_OK,
     COMPILER_FAILED_WITH_ERRORS
@@ -9,6 +124,8 @@ enum {
 struct compile_process {
     // The flags in regards to how this file should be compiled
     int flags;
+
+    struct pos pos;
     struct compile_process_input_file {
         FILE* fp;
         const char* abs_path;
@@ -19,5 +136,21 @@ struct compile_process {
 
 int compile_file(const char* filename, const char* out_filename, int flags);
 struct compile_process *compiler_process_create(const char *filename, const char *filename_out, int flags);
+
+char compile_process_next_char(struct lex_process* lex_process);
+char compile_process_peek_char(struct lex_process* lex_process);
+void compile_process_push_char(struct lex_process* lex_process, char c);
+
+struct lex_process* lex_process_create(struct compile_process* compiler, struct lex_process_functions* functions, void* private);
+void lex_process_free(struct lex_process* process);
+void* lex_process_private(struct lex_process* process);
+struct vector* lex_process_tokens(struct lex_process* process);
+
+int lex(struct lex_process* process);
+
+void compiler_error(struct compile_process* compiler, const char* msg, ...);
+void compiler_warning(struct compile_process* compiler, const char* msg, ...);
+
+bool token_is_keyword(struct token* token, const char* value);
 
 #endif
